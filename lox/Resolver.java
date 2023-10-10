ecdd9a8d958246e576558f95867e0ed59c096480
aucker <aucker@qq.com>
2023-10-10 17:44:50 +0800

    refactor: minor code reformat
    Signed-off-by: aucker <aucker@qq.com>

--- lox/Resolver.java
+++ lox/Resolver.java
@@ -143,7 +143,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	@Override
 	public Void visitVariableExpr(Expr.Variable expr) {
 		if (!scopes.isEmpty() &&
-			scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
+				scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
 			Lox.error(expr.name, "Can't read local variable in " +
 					"its own initializer.");
 		}
